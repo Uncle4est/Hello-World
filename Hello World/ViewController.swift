87583947c42fd1837872f769cd2553303fda1551
Uncle4est <hellos.fibroid.0a@icloud.com>
2022-09-19 17:20:49 +0300

    Traffic light setup

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var buttonText: UIButton!
+    
+    var x = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        buttonText.setTitle("Start", for: .normal)
     }
 
-
+    @IBAction func buttonPressed() {
+        
+        x += 1
+        
+        buttonText.setTitle("Next", for: .normal)
+        if x == 1 {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        } else if x == 2 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else if x == 3 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else {
+            x = 0
+            viewDidLoad()
+        }
+    }
+    
 }
-
