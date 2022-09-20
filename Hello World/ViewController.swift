487e22f3b8b45f7a795233e14d6291ca9ea9ce5f
Uncle4est <hellos.fibroid.0a@icloud.com>
2022-09-20 17:41:55 +0300

    Traffic light setup after lesson

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,45 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
+    
     @IBOutlet var buttonText: UIButton!
     
-    var x = 0
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLightView.alpha = 0.3
-        yellowLightView.alpha = 0.3
-        greenLightView.alpha = 0.3
-        
         buttonText.setTitle("Start", for: .normal)
-    }
-
-    @IBAction func buttonPressed() {
         
-        x += 1
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
         
-        buttonText.setTitle("Next", for: .normal)
-        if x == 1 {
-            redLightView.alpha = 1
-            yellowLightView.alpha = 0.3
-            greenLightView.alpha = 0.3
-        } else if x == 2 {
-            redLightView.alpha = 0.3
-            yellowLightView.alpha = 1
-            greenLightView.alpha = 0.3
-        } else if x == 3 {
-            redLightView.alpha = 0.3
-            yellowLightView.alpha = 0.3
-            greenLightView.alpha = 1
-        } else {
-            x = 0
-            viewDidLoad()
-        }
+        print("\(redLightView.frame.height)")
+        
     }
     
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+  
+        print("\(redLightView.frame.height)")
+    }
+    
+    @IBAction func buttonPressed() {
+        if buttonText.currentTitle == "Start" {
+            buttonText.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
+}
+
+extension ViewController {
+    enum CurrentLight {
+    case red, yellow, green
+    }
 }
